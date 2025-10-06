a050340ef8f7950ecca85b3b24a55f5598806014
Harshvardhan Soni <120377448+VardhanHarshSoni@users.noreply.github.com>
2025-10-06 21:03:08 +0530

    Updated calculator.c
    The required changes have been made in the code

--- calculator.c
+++ calculator.c
@@ -1,78 +1,105 @@
-#include<stdio.h>
-#include<ctype.h>
-#include<string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#define SIZE 100
 
-int main(){
-	char expression[100];
-	char nexp[100];
-	printf("Enter the Expression:");
-	fgets(expression, sizeof(expression), stdin);
-	expression[strcspn(expression, "\n")] = '\0';
-	int j=0;
-	int i;
-	
-	for(i = 0; expression[i] != '\0'; i++) {
-        if (!isspace(expression[i])) {
-            nexp[j++] = expression[i];
+int calculateResult(int numbers[] , char operators[] , int n , int o) {
+    int i , j , result;
+    for (i = 0 ; i < o ; i++) {
+        if (operators[i] == '/' || operators[i] == '*') {
+            if (operators[i] == '/' && numbers[i + 1] == 0) {
+                printf(" Error: division by zero\n ");
+                return 0;
+            }
+
+            if (operators[i] == '*')
+                result = numbers[i] * numbers[i + 1];
+            else
+                result = numbers[i] / numbers[i + 1];
+
+            numbers[i] = result;
+
+            for ( j = i + 1 ; j < n - 1 ; j++)
+                numbers[j] = numbers[j + 1];
+            for (j = i ; j < o - 1 ; j++)
+                operators[j] = operators[j + 1];
+
+            n--;
+            o--;
+            i--;
         }
     }
-    nexp[j]='\0';
-    
-    for(i=0; nexp[i]!='\0';i++){
-    	if(!isdigit(nexp[i]) && nexp[i]!='+' && nexp[i]!='-' && nexp[i]!='*'  && nexp[i]!='/' ){
-    		printf("Error: invalid expression \n");
-    		return 0;
-		}
-	}
-//    printf("Original: %s\n", expression);
-//    printf("Unspaced expresssion is : %s\n", nexp);
-	
-	int numbers[100];
-	int operators[100];
-	int n=0, o=0;
-	
-	for(i=0;nexp[i]!='\0';){
-		if(isdigit(nexp[i])){
-			int num=0;
-			while(isdigit(nexp[i])){
-				num = num*10 + (nexp[i] - '0');
-				i++;
-			}
-			numbers[n++]=num;	
-		}
-		else{
-			operators[o++]=nexp[i];
-			i++;
-		}
-	}
-	int result;
-	for(i=0;i<o;i++){
-		if(operators[i]=='*' || operators[i] == '/'){
-			if (operators[i] == '/' && numbers[i+1] == 0) {
-            printf("Error: division by zero \n");
-            return 0;
-		}
-		if (operators[i] == '*'){
-			result=numbers[i]*numbers[i+1];
-		}
-		else {
-			result=numbers[i]/numbers[i+1];
-		}
-		numbers[i]=result;
-		for (j = i+1; j < n-1; j++){
-			numbers[j] = numbers[j+1];}
-        for (j = i; j < o-1; j++){
-        	operators[j] = operators[j+1];}
-        n--; o--; i--;
-		}
-	}
-	
-	result=numbers[0];
-	for (i = 0; i < o; i++) {
-        if (operators[i] == '+'){
-			result += numbers[i+1];}
-        else result -= numbers[i+1];
+
+    result = numbers[0];
+    for (i = 0; i < o; i++) {
+        if (operators[i] == '+')
+            result += numbers[i + 1];
+        else
+            result -= numbers[i + 1];
     }
+    return result;
+}
+
+int main() {
+    char expression[SIZE];
+    char clearedexp[SIZE];
+    printf("Enter the Expression: ");
+    fgets(expression, sizeof(expression), stdin);
+    expression[strcspn(expression, "\n")] = '\0';
+
+    int j = 0 , i;
+    for (i = 0 ; expression[i] != '\0' ; i++) {
+        if (!isspace(expression[i])) {
+            clearedexp[j++] = expression[i];
+        }
+    }
+    clearedexp[j] = '\0';
+
+    if (j == 0) {
+        printf("Error: Empty Expression \n");
+        return 0;
+    }
+
+    for (i = 0 ; clearedexp[i] != '\0' ; i++) {
+        if ( !isdigit(clearedexp[i]) && clearedexp[i] != '+' &&
+            clearedexp[i] != '-' && clearedexp[i] != '*' &&
+            clearedexp[i] != '/') {
+            printf(" Error: Invalid Expression \n ");
+            return 0;
+        }
+    }
+
+    if (!isdigit(clearedexp[0]) || !isdigit(clearedexp[j - 1])) {
+        printf("Error: Expression cannot start or end with an operator \n");
+        return 0;
+    }
+
+    for ( i = 0 ; clearedexp[i] != '\0' ; i++ ) {
+        if (!isdigit(clearedexp[i]) && clearedexp[i + 1] != '\0' &&
+            !isdigit(clearedexp[i + 1])) {
+            printf("Error: Consecutive operators not allowed \n");
+            return 0;
+        }
+    }
+
+    int numbers[SIZE];
+    char operators[SIZE];
+    int n = 0, o = 0;
+    for ( i = 0 ; clearedexp[i] != '\0';) {
+        if (isdigit(clearedexp[i])) {
+            int num = 0;
+            while (isdigit(clearedexp[i])) {
+                num = num * 10 + (clearedexp[i] - '0');
+                i++;
+            }
+            numbers[n++] = num;
+        } else {
+            operators[o++] = clearedexp[i];
+            i++;
+        }
+    }
+
+    int result = calculateResult(numbers, operators , n , o);
     printf("Result: %d\n", result);
     return 0;
 }
