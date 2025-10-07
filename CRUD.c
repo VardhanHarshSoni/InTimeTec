829cd113d1a4dac47d860ee34786d6989d2a7c63
Harshvardhan Soni <120377448+VardhanHarshSoni@users.noreply.github.com>
2025-10-07 13:32:32 +0530

    Updated CRUD.c

--- CRUD.c
+++ CRUD.c
@@ -1,167 +1,232 @@
-#include<stdio.h>
-#include<ctype.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#define SIZE 200
 
-typedef struct{
-	int id;
-	char name[100];
-	int age;
+typedef struct {
+    int id;
+    char name[SIZE];
+    int age;
 } User;
 
-void createfile(const char *filename) {
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL) {
-        fp = fopen(filename, "w");
-        if (fp == NULL) {
-            perror("Error in creating file");
-            exit(1);
+int createFileIfNotExists(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        file = fopen(filename, "w");
+        if (file == NULL) {
+            perror("Error creating file");
+            return 1;
         }
-        fprintf(fp, "ID,Name,Age \n");
-        printf("File '%s' created \n", filename);
-    } 
-	else {
+        fprintf(file, " ID , Name , Age \n");
+        printf("File '%s' created successfully \n", filename);
+    } else {
         printf("File '%s' already exists \n", filename);
     }
-    fclose(fp);
+    fclose(file);
+    return 0;
 }
 
-void create(const char *filename) {
+int userExistsOrNot( const char *filename, int id ) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL){
+    	return 0;
+	} 
+    char line[SIZE];
+    fgets(line, sizeof(line), file); 
+    while (fgets(line, sizeof(line), file)) {
+        User u;
+        if (sscanf(line, "%d , %99[^,] , %d", &u.id, u.name, &u.age) == 3) {
+            if (u.id == id) {
+                fclose(file);
+                return 1;
+            }
+        }
+    }
+    fclose(file);
+    return 0;
+}
+
+int validatedIntegerValue(const char *prompt) {
+    int num;
+    char buffer[SIZE];
+    while (1) {
+        printf("%s", prompt);
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            printf("Error reading input  Try again \n");
+            continue;
+        }
+        if (sscanf(buffer, "%d", &num) == 1) {
+            if (num > 0)
+                return num;
+            else
+                printf("Invalid input Please enter a positive integer \n");
+        } else {
+            printf("Invalid input  Please enter a valid integer \n");
+        }
+    }
+}
+
+int isValidName(const char *name) {
+	int i;
+    for (i = 0; name[i] != '\0'; i++) {
+        if (!isalpha(name[i]) && name[i] != ' ') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int createUser(const char *filename) {
     User user;
-    printf("Enter ID: ");
-    scanf("%d", &user.id);
-    getchar();
+    user.id = validatedIntegerValue("Enter ID: ");
+    if (userExistsOrNot(filename, user.id)) {
+        printf("Error: User with ID %d already exists \n", user.id );
+        return 1;
+    }
     printf("Enter Name: ");
     fgets(user.name, sizeof(user.name), stdin);
     user.name[strcspn(user.name, "\n")] = '\0';
-	printf("Enter Age: ");
-    scanf("%d", &user.age);
-    FILE *fp = fopen(filename, "a");
-    if (fp == NULL) {
-        perror("Error opening file");
-        exit(1);
+    while (!isValidName(user.name) || strlen(user.name) == 0) {
+    	printf("Invalid name Only alphabets and spaces are allowed, Try again: ");
+    	fgets(user.name, sizeof(user.name), stdin);
+    	user.name[strcspn(user.name, "\n")] = '\0';
+	}
+    user.age = validatedIntegerValue("Enter Age: ");
+    if (user.age <= 0) {
+        printf("Invalid Age \n");
+        return 1;
     }
-    fprintf(fp, "%d,%s,%d\n", user.id, user.name, user.age);
-    fclose(fp);
-    printf("User with ID %d added\n", user.id);
+    FILE *file = fopen(filename, "a");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+    fprintf(file, "%d,%s,%d\n", user.id, user.name, user.age);
+    fclose(file);
+    printf("User with ID %d added successfully \n", user.id);
+    return 0;
 }
 
-void read(const char *filename) {
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL) {
+void readUsers(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
         perror("Error opening file");
         return;
     }
-    char line[200];
-    printf("\n----- Users List -----\n");
-    while (fgets(line, sizeof(line), fp)) {
+    char line[SIZE];
+    printf("\n    Users List     \n");
+    while (fgets(line, sizeof(line), file)) {
         printf("%s", line);
     }
-    printf("\n----------------------\n");
-    fclose(fp);
+    fclose(file);
 }
 
-void update(const char *filename) {
-    int searchId;
-    printf("Enter User ID to update: ");
-    scanf("%d", &searchId);
-    getchar();
-    FILE *fp = fopen(filename, "r");
+void updateUser(const char *filename) {
+    int searchId = validatedIntegerValue("Enter User ID to update: ");
+    FILE *file = fopen(filename, "r");
     FILE *temp = fopen("temp.txt", "w");
-    if (fp == NULL || temp == NULL) {
-        perror("Error in opening file");
-        exit(1);
+    if (file == NULL || temp == NULL) {
+        perror("Error opening file");
+        return 1;
     }
-    char line[200];
+    char line[SIZE];
     int found = 0;
-    if (fgets(line, sizeof(line), fp)) {
+    if (fgets(line, sizeof(line), file))
         fputs(line, temp);
-    }
-
-    while (fgets(line, sizeof(line), fp)) {
+    while (fgets(line, sizeof(line), file)) {
         User user;
-        sscanf(line, "%d,%99[^,],%d", &user.id, user.name, &user.age);
-        if (user.id == searchId) {
-            found = 1;
-            printf("Enter new Name:");
-            fgets(user.name, sizeof(user.name), stdin);
-            user.name[strcspn(user.name, "\n")] = '\0';
-            printf("Enter new Age :");
-            scanf("%d", &user.age);
-            fprintf(temp, "%d,%s,%d\n", user.id, user.name, user.age);
-            printf("User with ID %d updated\n", user.id);
+        if (sscanf(line , "%d , %99[^,] , %d" , &user.id, user.name, &user.age) == 3) {
+            if (user.id == searchId) {
+                found = 1;
+                printf("Enter new Name: ");
+                fgets(user.name, sizeof(user.name), stdin);
+                user.name[strcspn(user.name, "\n")] = '\0';
+                while (!isValidName(user.name) || strlen(user.name) == 0) {
+    				printf(" Invalid name! Only alphabets and spaces are allowed. Try again: ");
+    				fgets(user.name, sizeof(user.name), stdin);
+    				user.name[strcspn(user.name, "\n")] = '\0';
+				}
+                user.age = validatedIntegerValue("Enter new Age: ");
+                if (user.age <= 0) {
+                    printf("Invalid Age \n");
+                    fclose(file);
+                    fclose(temp);
+                    remove("temp.txt");
+                    return;
+                }
+                fprintf(temp, "%d, %s, %d \n" , user.id, user.name, user.age);
+                printf("User with ID %d updated successfully \n", user.id);
+            } else {
+                fputs(line, temp); 
+            }
         } else {
             fputs(line, temp);
         }
     }
-    fclose(fp);
+    fclose(file);
     fclose(temp);
     remove(filename);
     rename("temp.txt", filename);
-    if (!found) {
-        printf("User with ID %d does not exist\n", searchId);
-    }
+    if (!found)
+        printf("User with ID %d not found \n", searchId);
 }
 
-
-void deleter(const char *filename) {
-    int searchId;
-    printf("Enter User ID to delete : ");
-    scanf("%d", &searchId);
-    FILE *fp = fopen(filename, "r");
+void deleteUser(const char *filename) {
+    int searchId = validatedIntegerValue("Enter User ID to delete: ");
+    FILE *file = fopen(filename, "r");
     FILE *temp = fopen("temp.txt", "w");
-    if (fp == NULL || temp == NULL) {
+    if (file == NULL || temp == NULL) {
         perror("Error opening file");
-        exit(1);
+        return;
     }
-    char line[200];
+    char line[SIZE];
     int found = 0;
-    if (fgets(line, sizeof(line), fp)) {
+    if (fgets(line, sizeof(line), file))
         fputs(line, temp);
-    }
-    while (fgets(line, sizeof(line), fp)) {
+    while (fgets(line, sizeof(line), file)) {
         User user;
-        sscanf(line, "%d,%99[^,],%d", &user.id, user.name, &user.age);
-        if (user.id == searchId) {
-            found = 1;
-            printf("User with ID %d deleted\n", user.id);
-            continue;
+        if (sscanf(line, "%d , %99[^,] , %d", &user.id, user.name, &user.age) == 3) {
+            if (user.id == searchId) {
+                found = 1;
+                printf("User with ID %d deleted successfully \n ", user.id);
+                continue;
+            }
         }
         fputs(line, temp);
     }
-    fclose(fp);
+    fclose(file);
     fclose(temp);
     remove(filename);
     rename("temp.txt", filename);
-    if (!found) {
-        printf("User with ID %d is not found\n", searchId);
-    }
+    if (!found)
+        printf(" User with ID %d not found \n ", searchId);
 }
 
 int main() {
     const char *filename = "users.txt";
     int choice;
-    createfile(filename);
+    createFileIfNotExists(filename);
     do {
-        printf("1. CREATE\n");
-        printf("2. READ\n");
-        printf("3. UPDATE\n");
-        printf("4. DELETE\n");
+        printf("1. Create User\n");
+        printf("2. Read Users\n");
+        printf("3. Update User\n");
+        printf("4. Delete User\n");
         printf("5. Exit\n");
         printf("Enter your choice: ");
-        scanf("%d", &choice);
-        getchar();
+        choice = validatedIntegerValue("");
         switch (choice) {
-            case 1: create(filename);
+            case 1: createUser(filename); 
 					break;
-            case 2: read(filename);
-                	break;
-            case 3: update(filename);
-                	break;
-            case 4: deleter(filename);
-                	break;
-            case 5: break;
-            default:printf("Invalid choice. Try again.\n");
+            case 2: readUsers(filename); 
+					break;
+            case 3: updateUser(filename); 
+					break;
+            case 4: deleteUser(filename); 
+					break;
+            case 5: printf(" Exiting program \n "); 
+					break;
+            default: printf(" Invalid choice  Try again \n ");
         }
     } while (choice != 5);
     return 0;
